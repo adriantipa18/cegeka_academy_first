128fa2fa63a894499606b93c6217729dcb8a983f
Adrian <Adrian@DESKTOP-JE8H084>
2021-03-19 17:23:08 +0200

    homework2

---
+++ first_homework/homework2/GenericsExercise.Console/Program.cs
@@ -0,0 +1,15 @@
+﻿using GenericsExercise.Console.Given;
+using GenericsExercise.Console.Given.Services;
+
+namespace GenericsExercise.Console
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.mainMenu();
+
+        }
+    }
+}
